e310a2d1c0bd6fb284ff73c3bd72dffdaba83fd8
manuhg13 <manuhg13@gmail.com>
2022-10-09 11:38:47 +0000

    d

--- Tema 2/Practica3/Ejercicio2/ejercicio2.php
+++ Tema 2/Practica3/Ejercicio2/ejercicio2.php
@@ -8,11 +8,22 @@
     <title>Ejercicio 2</title>
 </head>
 <body>
-    <?
+    <?php
         include_once("../../../CSS/cabecera.html");
     ?>
     <?php
         include_once("../../../CSS/intro.html");
+        echo "<h1>Ejercicio 2</h1>";
+        echo "<h2>Crea una página a la que se le pase por url una variable con el nombre que quieras y muestre el valor de la variable, el tipo, si es numérico o no y si lo es, si es entero o float.</h2>";
+        foreach ($_GET as $i => $valor) {
+            echo "La variable " . $i . " es un " . gettype($_GET[$i]) - " con el valor de: " . $_GET[$i];
+            echo "<br>";
+            if (is_numeric($_GET[$i])){
+                echo $_GET[$i] . " es numerico y es un " . gettype($_GET[$i]);
+            } else {
+                echo $_GET[$i] . " no es numerico";
+            }
+       }
     ?>
 </body>
 </html>
