0eed183149a87c0ae49f3780c8efb40679202764
manuhg13 <manuhg13@gmail.com>
2022-10-09 12:07:56 +0000

    4

--- Tema 2/Practica3/Ejercicio2/ejercicio2.php
+++ Tema 2/Practica3/Ejercicio2/ejercicio2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/CSS/estilo.css">
+    <link rel="stylesheet" href="../../../CSS/estilo.css">
     <title>Ejercicio 2</title>
 </head>
 <body>
@@ -16,7 +16,7 @@
         echo "<h1>Ejercicio 2</h1>";
         echo "<h2>Crea una página a la que se le pase por url una variable con el nombre que quieras y muestre el valor de la variable, el tipo, si es numérico o no y si lo es, si es entero o float.</h2>";
         foreach ($_GET as $i => $valor) {
-            echo "La variable " . $i . " es un " . gettype($_GET[$i]) - " con el valor de: " . $_GET[$i];
+            echo "La variable " . $i . " es un " . gettype($_GET[$i]) - " con el valor de: " . $_GET[$i] ;
             echo "<br>";
             if (is_numeric($_GET[$i])){
                 echo $_GET[$i] . " es numerico y es un " . gettype($_GET[$i]);
