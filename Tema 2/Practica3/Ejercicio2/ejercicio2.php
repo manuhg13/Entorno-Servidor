358faf1fa7264af06988276b9e65d7b863c1e6ca
manuhg13 <manuhg13@gmail.com>
2022-10-10 17:44:24 +0000

    s

--- Tema 2/Practica3/Ejercicio2/ejercicio2.php
+++ Tema 2/Practica3/Ejercicio2/ejercicio2.php
@@ -15,16 +15,17 @@
         include_once("../../../CSS/intro.html");
         echo "<h1>Ejercicio 2</h1>";
         echo "<h2>Crea una página a la que se le pase por url una variable con el nombre que quieras y muestre el valor de la variable, el tipo, si es numérico o no y si lo es, si es entero o float.</h2>";
-        foreach ($_GET as $valor) {
-            # code...
-        }
-        if (is_numeric($GET['gatos'])) {
-            if(strpos($_GET['gatos'], '.')!== false){
-                echo ""
+        foreach ($_GET as $valor) {           
+            if (ctype_digit($valor)){
+                echo "Es un número ";
+                if (is_float($valor)){
+                    echo "float";
+                }else{
+                    echo "entero";
+                }
+            }else {
+                echo "No es un número";
             }
-            echo $_GET[$i] . " es numerico y es un " . gettype($_GET[$i]);
-        } else {
-            echo $_GET[$i] . " no es numerico";
         }
        
        include_once("../../../CSS/pie.html");
---
+++ Tema 2/Practica3/Ejercicio3/ejercicio3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../CSS/estilo.css">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+    <?php
+        include_once("../../../CSS/cabecera.html");
+    ?>
+    <?php
+        include_once("../../../CSS/intro.html");
+        
+    ?>
+
+    <?php
+        include_once("../../../CSS/pie.html");
+    ?>
+</body>
+</html>
