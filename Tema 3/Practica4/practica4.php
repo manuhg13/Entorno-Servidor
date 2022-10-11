b80a03c8bae30bc2c4d3728ca8675912d455b71a
Manuel <manuhg13@gmail.com>
2022-10-11 09:09:53 +0000

    d

--- Tema 3/Practica4/practica4.php
+++ Tema 3/Practica4/practica4.php
@@ -13,7 +13,7 @@
     ?>
     <?php
         include_once("../../CSS/intro.html");
-        echo "<h2>Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
+        echo "<h2>1. Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
         $lineas= 3;
         for ($i=1; $i <= $lineas; $i++) { 
             for ($espacios=1; $espacios<=$lineas-$i; $espacios++) { 
@@ -24,28 +24,47 @@
             } 
             echo "<br>";
         }
-        echo "<h2>Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
-        $rombo=5;
+        echo "<h2>2. Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
+        $rombo=3;
+        
         for ($i=1; $i <= $rombo ; $i++) { 
+            for ($espacios=1; $espacios<=$rombo-$i; $espacios++) { 
+                echo "&nbsp;&nbsp";
+            }
+            for ($asteriscos=1; $asteriscos <=($i*2)-1; $asteriscos++) {    
+                echo "*";
+            } 
+            echo "<br>";
+        }
+        for ($i=2; $i >= 1 ; $i--) { 
+            for ($espacios=1; $espacios<=3-$i; $espacios++) { 
+                echo "&nbsp;&nbsp";
+            }
+            for ($asteriscos=1; $asteriscos <=($i*2)-1; $asteriscos++) {    
+                echo "*";
+            } 
+            echo "<br>";
+        }
+        
+        echo "<h2>3. Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
+        $invertido=5;
+        for ($i=1; $i <=$invertido ; $i++) { 
             if ($i<4) {
-                for ($espacios=1; $espacios<=$rombo-$i; $espacios++) { 
+                for ($espacios=1; $espacios<=($invertido/2)-$i ; $espacios++) { 
                     echo "&nbsp;&nbsp";
                 }
-                for ($asteriscos=1; $asteriscos <=($i*2)-1; $asteriscos++) {    
-                    echo "*";
-                } 
-                echo "<br>";
+                for ($asteriscos=1; $asteriscos<= ($i*2)-1;$asteriscos++) {              
+                    if ($asteriscos==1 || $asteriscos) {
+                        echo "*";
+                    }
+                }
             } else {
-                for ($espacios=$rombo; $espacios<=$rombo-$i; $espacios++) { 
-                    echo "&nbsp;&nbsp";
-                }
-                for ($asteriscos=$rombo; $asteriscos<=($i*2)-1; $asteriscos++) {    
-                    echo "*";
-                } 
-                echo "<br>";
+               //for($espacios=1);
             }
             
         }
+
+        
         include_once("../../CSS/pie.html");
     ?>
 </body>
