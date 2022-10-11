eb951e8d4988d049ff1181079d0d0b4b332ac764
Manuel <manuhg13@gmail.com>
2022-10-11 07:52:01 +0000

    d

---
+++ Tema 3/Practica4/practica4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 4 || Manuel Hernández</title>
+</head>
+<body>
+<?php
+        include_once("../CSS/cabecera.html");
+    ?>
+    <?php
+        include_once("../CSS/intro.html");
+        echo "<h2>Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
+        
+        include_once("../CSS/pie.html");
+    ?>
+</body>
+</html>
