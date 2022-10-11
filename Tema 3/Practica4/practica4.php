4f47a27694cbcc734d60ac391c3ae943d897d7a0
Manuel <manuhg13@gmail.com>
2022-10-11 08:01:09 +0000

    d

--- Tema 3/Practica4/practica4.php
+++ Tema 3/Practica4/practica4.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../CSS/estilo.css">
     <title>Practica 4 || Manuel Hernández</title>
 </head>
 <body>
 <?php
-        include_once("../CSS/cabecera.html");
+        include_once("../../CSS/cabecera.html");
     ?>
     <?php
-        include_once("../CSS/intro.html");
+        include_once("../../CSS/intro.html");
         echo "<h2>Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
         $lineas= 3;
         for ($i=1; $i <= $lineas; $i++) { 
@@ -24,8 +25,9 @@
             echo "<br>";
         }
         echo "<h2>Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
-        
-        include_once("../CSS/pie.html");
+        $rombo=5;
+
+        include_once("../../CSS/pie.html");
     ?>
 </body>
 </html>
