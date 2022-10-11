c133d1bffaef864e0c81ab02c9cb6ad1e41f6be7
Manuel <manuhg13@gmail.com>
2022-10-11 07:57:32 +0000

    f

--- Tema 3/Practica4/practica4.php
+++ Tema 3/Practica4/practica4.php
@@ -13,6 +13,17 @@
     <?php
         include_once("../CSS/intro.html");
         echo "<h2>Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
+        $lineas= 3;
+        for ($i=1; $i <= $lineas; $i++) { 
+            for ($espacios=1; $espacios<=$lineas-$i; $espacios++) { 
+                echo "&nbsp;&nbsp";
+            }
+            for ($asteriscos=1; $asteriscos <=($i*2)-1; $asteriscos++) {    
+                echo "*";
+            } 
+            echo "<br>";
+        }
+        echo "<h2>Realiza un programa utilizando bucles qie muestre la siguiente figura</h2>";
         
         include_once("../CSS/pie.html");
     ?>
