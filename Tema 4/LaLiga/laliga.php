ebfec5ab2325716f2f218ded1eb5501e299e1fcf
Manuel <manuhg13@gmail.com>
2022-10-17 06:58:52 +0000

    f

---
+++ Tema 4/LaLiga/laliga.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $liga =
+        array(
+            "Zamora" =>  array(
+                "Salamanca" => array(
+                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Avila" => array(
+                    "Resultado" => "4-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Valladolid" => array(
+                    "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 1, "Penalti" => 1
+                )
+            ),
+            "Salamanca" =>  array(
+                "Zamora" => array(
+                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Avila" => array(
+                    "Resultado" => "4-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Valladolid" => array(
+                    "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 2, "Penalti" => 1
+                )
+            ),
+            "Avila" =>  array(
+                "Zamora" => array(
+                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 2
+                ),
+                "Salamanca" => array(
+                    "Resultado" => "1-3", "Roja" => 1, "Amarilla" => 3, "Penalti" => 0
+                ),
+                "Valladolid" => array(
+                    "Resultado" => "1-3", "Roja" => 1, "Amarilla" => 0, "Penalti" => 1
+                )
+            ),
+            "Valladolid" =>  array(
+                "Zamora" => array(
+                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Salamanca" => array(
+                    "Resultado" => "3-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Avila" => array(
+                    "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 1, "Penalti" => 2
+                )
+            ),
+        );
+    
+    ?>
+</body>
+</html>
