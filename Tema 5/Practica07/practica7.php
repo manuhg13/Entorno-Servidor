9cc11b29b571144200ba5ebac8605237bd287365
Manuel <manuhg13@gmail.com>
2022-10-20 12:01:14 +0000

    d

---
+++ Tema 5/Practica07/practica7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../CSS/estilo.css">
+    <title>Practica 7 || Manuel Hernandez</title>
+</head>
+<body>
+    <?php
+        include_once("../../CSS/cabecera.html");
+    ?>
+    <?php
+        include_once("../../CSS/cabecera.html");
+    ?>
+</body>
+</html>
