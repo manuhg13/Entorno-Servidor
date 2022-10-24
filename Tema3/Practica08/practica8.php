ad6e7c40f17656e4b1f18ac36bce1119adfd237d
Manuel <manuhg13@gmail.com>
2022-10-24 12:39:18 +0000

    d

---
+++ Tema3/Practica08/practica8.php
@@ -0,0 +1,17 @@
+<?php
+    require("validador.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 8 || Manuel Hernández Gómez</title>
+</head>
+<body>
+    <form action="./practica8.php" method="post" enctype="multipart/form-data">
+        <p></p>
+    </form>
+</body>
+</html>
