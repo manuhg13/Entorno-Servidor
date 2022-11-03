0160f4c7ca5f686eb1bf96b6f154437667d1e499
Manuel <manuhg13@gmail.com>
2022-11-03 12:13:09 +0000

    d

---
+++ Tema3/ExpresionesRegu/expreRegu.php
@@ -0,0 +1,20 @@
+<?php
+    $cadena = 'Hoy hace muy buen día y hay nubes';
+    $patron= '/hace/';
+
+    echo "<br> Cadena: " . $cadena . " y patron: " . $patron .  " Match: " . preg_match($patron,$cadena);
+
+    $patron="/ha./";
+    
+    echo "<br> Cadena: " . $cadena . " || patron: " . $patron .  " ||Match: " . preg_match($patron,$cadena);
+    
+    $patron='/ha.\s/';
+    
+    echo "<br> Cadena: " . $cadena . " || patron: " . $patron .  " ||Match: " . preg_match($patron,$cadena);
+
+
+
+
+
+
+?>
