1600eaf00cc0bda784a21af739cc50292c413ec6
Manuel <manuhg13@gmail.com>
2022-11-07 08:13:46 +0000

    d

--- Tema3/Practica09/practica09.php
+++ Tema3/Practica09/practica09.php
@@ -7,6 +7,29 @@
     <title>Practica 09 || ManuelHG</title>
 </head>
 <body>
+    <?php
+        include_once("../../CSS/cabecera.html");
+        include_once("../../CSS/intro.html");
+    ?>
+    <h1>Formulario</h1>
+    <?php
+        if (validarTodo()){
+            imprimirInfo();
+        }else{
     
+    ?>
+    <form action="./practica09.php" method="post" enctype="multipart/form-data">
+        <p>
+            <label for="idNombre">Nombre: </label>
+            <input type="text" name="nombre" id="idNombre" placeholder="Nombre" value="<?php
+                if (enviado() && !vacio('nombre')){
+                    echo $_REQUEST['nombre'];
+                }
+            ?>">
+        </p>
+    </form>
+    <?php
+        }
+    ?>
 </body>
 </html>
