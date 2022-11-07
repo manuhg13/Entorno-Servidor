b624e283824ba8f2d7b6c183ee81f8f704d336d1
Manuel <manuhg13@gmail.com>
2022-11-07 09:14:55 +0000

    d

--- Tema3/Practica09/libreria.php
+++ Tema3/Practica09/libreria.php
@@ -33,4 +33,15 @@
         }
         return false;
     }
+
+    function patFecha(){
+        $patron='/^([0-2][1-9]|3[0-1]\/[1-12]{1,2}\/\d{1,4})$/';
+        if(preg_match($patron,$_REQUEST['fecha'])==1){
+            $cortado=explode('/',$_REQUEST['fecha']);
+            if(checkdate($cortado[1],$cortado[0],$cortado[2])){
+                return true;
+            }
+        }
+        return false;
+    }
 ?>
