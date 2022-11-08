28bbe4094b46e206dcdd091607dc4fe777e1eaa1
Manuel <manuhg13@gmail.com>
2022-11-08 08:03:16 +0000

    f

--- Tema3/AccesoADatos/ficheros.php
+++ Tema3/AccesoADatos/ficheros.php
@@ -1,8 +1,14 @@
 <?php
     
-    //Abrir si existe 
-
-
+    /*Abrir fichero SI existe 
+    Si se va a abrir para lectura con r 
+    Comprobar que existe antes*/
+    
+    //Leer
+    if (file_exists('miarchivo.txt'))
+        echo "<br> Existe";
+    else 
+        echo "<br> No existe";
 
 
 
