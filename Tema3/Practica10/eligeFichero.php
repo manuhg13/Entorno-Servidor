cacdce33db7b0f2af57ecfdb2a12f3af97f042c3
Manuel <manuhg13@gmail.com>
2022-11-10 12:25:03 +0000

    d

--- Tema3/Practica10/eligeFichero.php
+++ Tema3/Practica10/eligeFichero.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,7 +31,7 @@
         <p>
             <label for="idFichero">Nombre: </label>
             <input type="text" name="fichero" id="idFichero">
-            
+
             <input type="submit" value="editar" name="editar">
             <input type="submit" value="leer" name="leer">
         </p>
