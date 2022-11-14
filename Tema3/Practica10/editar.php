634b2493d82f5e757827b9cc0c9b085547d87612
Manuel <manuhg13@gmail.com>
2022-11-14 08:43:31 +0000

    d

--- Tema3/Practica10/editar.php
+++ Tema3/Practica10/editar.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../CSS/estilo.css">
     <title>Editar fichero</title>
 </head>
 <body>
+    <?php
+        include_once("../../CSS/cabecera.html");
+        include_once("../../CSS/intro.html");
+    ?>
     <?php
         require("validador.php");
         if (enviado()){
