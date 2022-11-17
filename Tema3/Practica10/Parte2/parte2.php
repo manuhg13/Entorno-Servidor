098d3a798848a4de89ba572eaaf5448a33618c55
Manuel <manuhg13@gmail.com>
2022-11-17 13:07:53 +0000

    d

--- Tema3/Practica10/Parte2/parte2.php
+++ Tema3/Practica10/Parte2/parte2.php
@@ -7,30 +7,41 @@
     <title>PR 10 || Parte 2</title>
 </head>
 <body>
-    <table border="1">
-        <tr>
-            <th>Alumno</th>
-            <th>Nota 1</th>
-            <th>Nota 2</th>
-            <th>Nota 3</th>
-            <th></th>
-        </tr>
-        <?php
-        $fila=1;
-            if (($abrir= fopen("notas.csv", "r"))){
-                $j=0;
-                while ($datos = fgetcsv($abrir,filesize('notas.csv'),';')){
-                    $num= count($datos);
-                    echo "<tr>";
-                    for ($i=0; $i < $num; $i++) { 
-                        echo "<td>" . $datos[$i] . "</td>";
-                    }
-                    echo '<td> <input type="submit" name="editar' . $j++ .'" value="Editar"></td>';
-                    echo "</tr>";
+    <?php
 
+    ?>
+
+    <form action="./">
+        <input type="hidden" name="fila" value="<?php
+            echo $datos;
+        ?>">
+        <table border="1">
+            <tr>
+                <th>Alumno</th>
+                <th>Nota 1</th>
+                <th>Nota 2</th>
+                <th>Nota 3</th>
+                <th></th>
+            </tr>
+            <?php
+            $fila=1;
+            $todos=array();
+                if (($abrir= fopen("notas.csv", "r"))){
+                    $j=0;
+                    while ($datos = fgetcsv($abrir,filesize('notas.csv'),';')){
+                        $num= count($datos);
+                        array_push($todos,$datos);
+                        echo "<tr>";
+                        for ($i=0; $i < $num; $i++) { 
+                            echo "<td>" . $datos[$i] . "</td>";
+                        }
+                        echo '<td> <input type="submit" name="editar' . $j++ .'" value="Editar"></td>';
+                        echo "</tr>";
+    
+                    }
                 }
-            }
-        ?>
-    </table>
+            ?>
+        </table>
+    </form>
 </body>
 </html>
