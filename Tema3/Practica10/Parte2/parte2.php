a874df29448b1979dace4bf5ce5749b489dfbf5c
Manuel <manuhg13@gmail.com>
2022-11-17 12:44:46 +0000

    d

---
+++ Tema3/Practica10/Parte2/parte2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PR 10 || Parte 2</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>Alumno</th>
+            <th>Nota 1</th>
+            <th>Nota 2</th>
+            <th>Nota 3</th>
+            <th></th>
+        </tr>
+        <?php
+        $fila=1;
+            if (($abrir= fopen("notas.csv", "r"))){
+                while ($datos = fgetcsv($abrir,filesize('notas.csv'),';')){
+                    $num= count($datos);
+                    echo "<tr>";
+                    for ($i=0; $i < $num; $i++) { 
+                        echo "<td>" . $datos[$i] . "</td>";
+                    }
+                    echo '<td> <input type="submit" name="editar" value="Editar"></td>';
+                    echo "</tr>";
+
+                }
+            }
+        ?>
+    </table>
+</body>
+</html>
