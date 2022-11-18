b9d683cd3bb688b86111ddfda9ae4cd1c45e98a0
Manuel <manuhg13@gmail.com>
2022-11-18 12:53:38 +0000

    8

--- Tema3/Practica10/Parte2/parte2.php
+++ Tema3/Practica10/Parte2/parte2.php
@@ -4,17 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../CSS/estilo.css">
     <title>PR 10 || Parte 2</title>
 </head>
 <body>
     <?php
-        
+        include("../../../CSS/cabecera.html");
     ?>
-
-    <form action="./">
-        <input type="hidden" name="todos" value="<?php
-            echo $todos;
-        ?>">
         <table border="1">
             <tr>
                 <th>Alumno</th>
@@ -36,7 +32,7 @@
                             echo "<td>" . $datos[$i] . "</td>";
                         }
                         //echo '<td> <input type="submit" name="' . $j++ .'" value="Editar"></td>';
-                        echo "<td><a href='./edita.php?numero='". $j++ . " ></td>";
+                        echo '<td><a href="./edita.php?numero='. $j++ . '" class="colorin">Editar</td>';
                         echo "</tr>";
     
                     }
@@ -44,6 +40,5 @@
                 }
             ?>
         </table>
-    </form>
 </body>
 </html>
