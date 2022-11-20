760f03f8c866aa1929e8bd6513c0d8331e8062e4
manuhg13 <manuhg13@gmail.com>
2022-11-20 10:56:16 +0000

    d

--- Tema3/Practica10/Parte2/edita.php
+++ Tema3/Practica10/Parte2/edita.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../CSS/estilo.css">
     <title>Editar tabla</title>
 </head>
 <body>
@@ -22,17 +23,20 @@
             include("../../../CSS/cabecera.html");
             require("validador.php");
     
-            if (enviado()){
-                $todos[$_REQUEST['indice']][1]=$_REQUEST['nota1'];
-                $todos[$_REQUEST['indice']][2]=$_REQUEST['nota2'];
-                $todos[$_REQUEST['indice']][3]=$_REQUEST['nota3'];
+            if (validarTodo()){               
+                    $todos[$_REQUEST['indice']][1]=$_REQUEST['nota1'];
+                    $todos[$_REQUEST['indice']][2]=$_REQUEST['nota2'];
+                    $todos[$_REQUEST['indice']][3]=$_REQUEST['nota3'];
 
-                if ($abrir=fopen('notas.csv','w')){
-                    foreach($todos as $campos){
-                        fputcsv($abrir,$campos,";");
+                    if ($abrir=fopen('notas.csv','w')){
+                        foreach($todos as $campos){
+                            fputcsv($abrir,$campos,";");
+                        }
                     }
-                }
-                fclose($abrir);
+                    fclose($abrir);
+
+                    header('Location: ./parte2.php');
+                    exit();    
             }else{
     
         ?>
@@ -51,18 +55,51 @@
             <input type="text" name="nota1" id="idNota1" value="<?php
                 echo $todos[$_REQUEST['indice']][1];
             ?>">
+
+            <?php
+                if (enviado()) {
+                    if(vacio('nota1')){
+                        echo "<p style='color: red'>Introduce una nota</p>";
+                    }elseif(!patNotas('nota1')){
+                        echo "<p style='color: red'>Nota incorrecta</p>";
+                    }
+                }
+                
+            ?>
         </p>
         <p>
             <label for="idNota2">Nota2</label>
             <input type="text" name="nota2" id="idNota2" value="<?php
                 echo $todos[$_REQUEST['indice']][2];
             ?>">
+
+            <?php
+                if (enviado()) {
+                    if(vacio('nota2')){
+                        echo "<p style='color: red'>Introduce una nota</p>";
+                    }elseif(!patNotas('nota2')){
+                        echo "<p style='color: red'>Nota incorrecta</p>";
+                    }
+                }
+                
+            ?>
         </p>
         <p>
             <label for="idNota3">Nota3</label>
             <input type="text" name="nota3" id="idNota3" value="<?php
                 echo $todos[$_REQUEST['indice']][3];
             ?>">
+
+            <?php
+                if (enviado()) {
+                    if(vacio('nota3')){
+                        echo "<p style='color: red'>Introduce una nota</p>";
+                    }elseif(!patNotas('nota3')){
+                        echo "<p style='color: red'>Nota incorrecta</p>";
+                    }
+                }
+                
+            ?>
         </p>
 
         <input type="submit" value="Guardar" name="guardar" class="colorin">
--- Tema3/Practica10/Parte2/validador.php
+++ Tema3/Practica10/Parte2/validador.php
@@ -7,4 +7,32 @@
         return false;
     }
 
+    function patNotas($nota){
+        $patron= '/^(10|\d)$/';
+        if (preg_match($patron,$_REQUEST[$nota])==1){
+            return true;
+        }
+        return false;
+    }
+
+    function vacio($dato){
+        if (empty($_REQUEST[$dato])) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    function validarTodo(){
+        if (enviado()){
+            if (!vacio('nota1') && patNotas('nota1')){
+                if(!vacio('nota2') && patNotas('nota2')){
+                    if(!vacio('nota3') && patNotas('nota3')){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 ?>
