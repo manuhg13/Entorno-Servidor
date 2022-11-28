b3705c87a7307f97108fb74a65dd1ee1316cff28
Manuel <manuhg13@gmail.com>
2022-11-28 11:34:13 +0000

    7

---
+++ Tema4/Seguro/conexion.php
@@ -0,0 +1,11 @@
+<?php
+
+    define('HOST', '192.168.2.205');
+    define('USER', 'manuel');
+    define('PASS', 'manuel');
+    define('BBDD', '');
+
+    
+
+
+?>
---
+++ Tema4/mysqlli.php
@@ -0,0 +1,7 @@
+<?php
+    require('./Seguro/conexion.php');
+
+    $conexion= mysqli_connect(HOST,USER,PASS);
+
+    echo "";
+?>
