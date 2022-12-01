c638defe8b6eda1c4e2349636c793a4cd14311e1
Manuel <manuhg13@gmail.com>
2022-12-01 13:20:22 +0000

    d

--- Tema4/mysqlli.php
+++ Tema4/mysqlli.php
@@ -1,7 +1,57 @@
 <?php
     require('./Seguro/conexion.php');
     try {
-        $conexion= mysqli_connect(HOST,USER,PASS);
+        /*-----Con funciones----*/ 
+        $conexion= mysqli_connect($_SERVER['SERVER_ADDR'],USER,PASS,'mundial');
+        
+        $sql='select * from equipo';
+        $resultado= mysqli_query($conexion,$sql);
+
+
+        while($fila = $resultado->fetch_array()){
+            echo $fila['id'] . " ";
+            echo $fila['nombre']. '<br>';
+        }
+        
+        mysqli_close($conexion);
+        /*------> Con Objetos--------
+        $conexion0 = new mysqli();
+        $conexion0->connect($_SERVER['SERVER_ADDR'],USER,PASS,'mundial');
+        $conexion0->close();*/
+    } catch (Exception $ex) {
+        if ($ex->getCode()==1045){
+            echo "Credenciales incorrectas";
+        }
+        if ($ex->getCode()==2002){
+            echo "Acabado tiempo de conexión";
+        }       
+        if ($ex->getCode()==1049){
+            echo "No existe la base de datos no existe";
+        }       
+    }
+
+    try {
+        /*-----Consultas preparadas----*/ 
+        $conexion= mysqli_connect($_SERVER['SERVER_ADDR'],USER,PASS,'mundial');
+
+        
+        $sql='select * from equipo where id = ? and nombre = ?';
+
+        $consultaPreparada= mysqli_stmt_init($conexion);
+        mysqli_stmt_prepare($consultaPreparada,$sql);
+
+        $id=1;
+        $nombre='España';
+
+        mysqli_stmt_bind_param($consultaPreparada,'i',$id);
+        mysqli_stmt_bind_param($consultaPreparada,'s',$nombre);
+
+        mysqli_stmt_execute($consultaPreparada);
+
+
+        
+        
+        mysqli_close($conexion);
         
     } catch (Exception $ex) {
         if ($ex->getCode()==1045){
@@ -10,7 +60,14 @@
         if ($ex->getCode()==2002){
             echo "Acabado tiempo de conexión";
         }       
+        if ($ex->getCode()==1049){
+            echo "No existe la base de datos no existe";
+        }       
     }
 
-    echo "Va perfe";
+
+
+    
+
+
 ?>
