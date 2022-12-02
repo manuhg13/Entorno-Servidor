95545ea216cdec5d550749fd8af47db516c1ce87
Manuel <manuhg13@gmail.com>
2022-12-02 12:25:17 +0000

    d

--- Tema4/mysqlli.php
+++ Tema4/mysqlli.php
@@ -29,6 +29,7 @@
             echo "No existe la base de datos no existe";
         }       
     }
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
     try {
         /*-----Consultas preparadas----*/ 
@@ -43,14 +44,16 @@
         $id=1;
         $nombre='España';
 
-        mysqli_stmt_bind_param($consultaPreparada,'i',$id);
-        mysqli_stmt_bind_param($consultaPreparada,'s',$nombre);
-
+        mysqli_stmt_bind_param($consultaPreparada,'is',$id,$nombre);
         mysqli_stmt_execute($consultaPreparada);
 
+        mysqli_stmt_bind_result($consultaPreparada,$r_id,$r_nom);
+        
 
-        
-        
+        while (mysqli_stmt_fetch($consultaPreparada)) {
+            echo "<br> ID: " . $r_id .  " Nombre: " . $r_nom;
+        }
+  
         mysqli_close($conexion);
         
     } catch (Exception $ex) {
@@ -63,6 +66,119 @@
         if ($ex->getCode()==1049){
             echo "No existe la base de datos no existe";
         }       
+    }
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    try {
+        /*-----Consultas preparadas insert----*/ 
+        $conexion= mysqli_connect($_SERVER['SERVER_ADDR'],USER,PASS,'mundial');
+
+        
+        $sql='insert into equipo values (?,?)';
+
+        $consultaPreparada= mysqli_stmt_init($conexion);
+        mysqli_stmt_prepare($consultaPreparada,$sql);
+
+        $id=3;
+        $nombre='Brasil';
+
+        mysqli_stmt_bind_param($consultaPreparada,'is',$id,$nombre);
+        mysqli_stmt_execute($consultaPreparada);
+        echo mysqli_stmt_num_rows($consultaPreparada);
+
+
+        //mysqli_stmt_bind_result($consultaPreparada,$r_id,$r_nom);
+        
+
+        /*while (mysqli_stmt_fetch($consultaPreparada)) {
+            echo "<br> ID: " . $r_id .  " Nombre: " . $r_nom;
+        }*/
+  
+        mysqli_close($conexion);
+        
+    } catch (Exception $ex) {
+        if ($ex->getCode()==1045){
+            echo "Credenciales incorrectas";
+        }
+        if ($ex->getCode()==2002){
+            echo "Acabado tiempo de conexión";
+        }       
+        if ($ex->getCode()==1049){
+            echo "No existe la base de datos no existe";
+        }       
+    }
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    try {
+        /*-----Consultas preparadas update----*/ 
+        $conexion= mysqli_connect($_SERVER['SERVER_ADDR'],USER,PASS,'mundial');
+
+        
+        $sql="update equipo set nombre= ? where nombre='Brasil'";
+
+        $consultaPreparada= mysqli_stmt_init($conexion);
+        mysqli_stmt_prepare($consultaPreparada,$sql);
+
+        $nombre='Japon';
+
+        mysqli_stmt_bind_param($consultaPreparada,'s',$nombre);
+        echo mysqli_stmt_affected_rows($consultaPreparada);
+        mysqli_stmt_execute($consultaPreparada);
+
+
+        //mysqli_stmt_bind_result($consultaPreparada,$r_id,$r_nom);
+        
+
+        /*while (mysqli_stmt_fetch($consultaPreparada)) {
+            echo "<br> ID: " . $r_id .  " Nombre: " . $r_nom;
+        }*/
+  
+        mysqli_close($conexion);
+        
+    } catch (Exception $ex) {
+        if ($ex->getCode()==1045){
+            echo "Credenciales incorrectas";
+        }
+        if ($ex->getCode()==2002){
+            echo "Acabado tiempo de conexión";
+        }       
+        if ($ex->getCode()==1049){
+            echo "No existe la base de datos no existe";
+        }       
+    }
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    try {
+        /*-----Consultas preparadas update----*/ 
+        $conexion= mysqli_connect($_SERVER['SERVER_ADDR'],USER,PASS,'mundial');
+
+        
+        $sql="insert into equipo values (4,'Australia');";
+        $sql1="insert into equipo values (5,'Argentina');";
+        $sql2="insert into equipo values (6,'Croacia');";
+        $sql2="insert into equipo values (6,'Islandia');";
+
+        mysqli_query($conexion,$sql);
+        mysqli_query($conexion,$sql1);
+        mysqli_query($conexion,$sql2);
+
+        mysqli_autocommit($conexion,false);
+
+
+    } catch (Exception $ex) {
+        if ($ex->getCode()==1045){
+            echo "Credenciales incorrectas";
+        }
+        if ($ex->getCode()==2002){
+            echo "Acabado tiempo de conexión";
+        }       
+        if ($ex->getCode()==1049){
+            echo "No existe la base de datos no existe";
+        }       
+        mysqli_rollback($conexion);
+    } finally{
+        mysqli_close($conexion);
+        
     }
 
 
