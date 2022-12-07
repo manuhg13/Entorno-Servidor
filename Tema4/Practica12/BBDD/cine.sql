c3cbc024022c2661548464dd49ca87b00887e559
manuel <manuhg13@gmail.com>
2022-12-07 12:40:43 +0000

    q

--- Tema4/Practica12/BBDD/cine.sql
+++ Tema4/Practica12/BBDD/cine.sql
@@ -1,5 +1,6 @@
 create database cine;
 use cine;
+grant all on cine.* to manu;
 
 create table mejorPelicula(
     titulo VARCHAR(90) primary key, 
@@ -11,13 +12,13 @@ create table mejorPelicula(
 )engine =innodb;
 
 INSERT INTO mejorPelicula VALUES ('El discurso del rey','Tom Hooper','Drama historico','2010-6-9','4','8.0');
-INSERT INTO mejorPelicula VALUES ('El artista','Michel Hazanavicius','Comedia romántica','2011-15-5','5','7.9');
-INSERT INTO mejorPelicula VALUES ('Argo','Ben Affleck','Drama/Suspense','2012-4-11','3','7.7');
-INSERT INTO mejorPelicula VALUES ('12 años de esclavitud','Steve McQueen','Biografía/Drama','2013-6-9','3','8.1');
-INSERT INTO mejorPelicula VALUES ('Birdman','Alejandro González Iñárritu','Comedia dramática','2014-19-10','4','7.7');
-INSERT INTO mejorPelicula VALUES ('Spotlight','Tom McCarthy','Biografía/Drama','2015-8-10','2','8.1');
-INSERT INTO mejorPelicula VALUES ('Moonlight','Barry Jenkins','Coming-of-age / Drama','2016-2-9','3','7.4');
-INSERT INTO mejorPelicula VALUES ('La forma del agua','Guillermo del Toro','Fantasía/Romance','2017-31-8','4','7.3');
-INSERT INTO mejorPelicula VALUES ('Green Book','Peter Farrelly','Biografía / Comedia dramática / Road movie ','2018-16-11','4','8.2');
-INSERT INTO mejorPelicula VALUES ('Parásitos','Bong Joon-ho','Drama/Suspenso/Humor negro ','2019-21-5','4','8.5');
+INSERT INTO mejorPelicula VALUES ('El artista','Michel Hazanavicius','Comedia romántica','2011-5-15','5','7.9');
+INSERT INTO mejorPelicula VALUES ('Argo','Ben Affleck','Drama/Suspense','2012-11-4','3','7.7');
+INSERT INTO mejorPelicula VALUES ('12 años de esclavitud','Steve McQueen','Biografía/Drama','2013-9-6','3','8.1');
+INSERT INTO mejorPelicula VALUES ('Birdman','Alejandro González Iñárritu','Comedia dramática','2014-10-19','4','7.7');
+INSERT INTO mejorPelicula VALUES ('Spotlight','Tom McCarthy','Biografía/Drama','2015-10-8','2','8.1');
+INSERT INTO mejorPelicula VALUES ('Moonlight','Barry Jenkins','Coming-of-age / Drama','2016-9-2','3','7.4');
+INSERT INTO mejorPelicula VALUES ('La forma del agua','Guillermo del Toro','Fantasía/Romance','2017-8-31','4','7.3');
+INSERT INTO mejorPelicula VALUES ('Green Book','Peter Farrelly','Biografía / Comedia dramática / Road movie ','2018-11-16','4','8.2');
+INSERT INTO mejorPelicula VALUES ('Parásitos','Bong Joon-ho','Drama/Suspenso/Humor negro ','2019-5-21','4','8.5');
 
--- Tema4/Practica12/leerTabla.php
+++ Tema4/Practica12/leerTabla.php
@@ -7,35 +7,53 @@
     <title>Leer tabla || PR 12 </title>
 </head>
 <body>
-    <?php
-        require('conexionBD.php');
-        try {
-            
-            $conexion= mysqli_connect($_SERVER['SERVER_ADDR'],USER,PASS,BBDD);
-            
-            $sql='select * from equipo';
-            $resultado= mysqli_query($conexion,$sql);
-    
-    
-            while($fila = $resultado->fetch_array()){
-                echo $fila['id'] . " ";
-                echo $fila['nombre']. '<br>';
+
+    <table border="1">
+        <tr>
+            <th>Título</th>
+            <th>Director</th>
+            <th>Género</th>
+            <th>Estreno</th>
+            <th>Nominaciones</th>
+            <th>Nota</th>
+        </tr>   
+        <?php
+            require('conexionBD.php');
+            try {
+                
+                $conexion= mysqli_connect($_SERVER['SERVER_ADDR'],USER,PASS,BBDD);
+                
+                $sql='select * from mejorPelicula';
+                $resultado= mysqli_query($conexion,$sql);
+        
+        
+                while($fila = $resultado->fetch_array()){
+                    echo "<tr>";
+                    echo "<td>". $fila['titulo'] . "</td> ";
+                    echo "<td>". $fila['director'] . "</td> ";
+                    echo "<td>". $fila['genero'] . "</td> ";
+                    echo "<td>". $fila['estreno'] . "</td> ";
+                    echo "<td>". $fila['nominaciones'] . "</td> ";
+                    echo "<td>". $fila['nota'] . "</td> ";
+                    echo "</tr>";
+                }
+                
+                mysqli_close($conexion);
+                
+            } catch (Exception $ex) {
+                if ($ex->getCode()==1045){
+                    echo "Credenciales incorrectas";
+                }
+                if ($ex->getCode()==2002){
+                    echo "Acabado tiempo de conexión";
+                }       
+                if ($ex->getCode()==1049){
+                    echo "No existe la base de datos no existe";
+                }       
             }
-            
-            mysqli_close($conexion);
-            
-        } catch (Exception $ex) {
-            if ($ex->getCode()==1045){
-                echo "Credenciales incorrectas";
-            }
-            if ($ex->getCode()==2002){
-                echo "Acabado tiempo de conexión";
-            }       
-            if ($ex->getCode()==1049){
-                echo "No existe la base de datos no existe";
-            }       
-        }
-    
-    ?>
+        
+        ?>
+
+    </table>
 </body>
 </html>
