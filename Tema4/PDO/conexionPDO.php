823f042a072b53268b9b718e119c37c03597b2d2
Manuel <manuhg13@gmail.com>
2022-12-13 08:50:37 +0000

    d

--- Tema4/PDO/conexionPDO.php
+++ Tema4/PDO/conexionPDO.php
@@ -1,12 +1,31 @@
 <?
-    require('../Practica12/conexionBD.php');
+    require('pdo.php');
 
     //conexión
 
     try {
-        $conexion= new PDO('mysql:host=' . $_SERVER['SERVER_ADDR']. ';dbname=' . BBDD,USER,PASS);
+        $conexion= new PDO('mysql:host=' . $_SERVER['SERVER_ADDR']. ';dbname=' .BBDD,USER,PASS);
 
+
+        //echo $conexion->getAttribute(PDO::ATTR_CLIENT_VERSION);
+        
         echo 'Funciona';
+        echo '<br><br>';
+        
+        $sql="insert into equipo values('8','Lituania');";
+        $sql2="select * from equipo;";
+        
+        $conexion->exec($sql);
+        
+        $resultado=$conexion->query($sql2);
+        
+        echo '<br><br>';
+
+        while ($fila = $resultado->fetch(PDO::FETCH_BOTH)) {
+            echo "<pre>";
+            print_r($fila);
+            echo "</pre>";
+        }
     } catch (Exception $ex) {
         if ($ex->getCode()==1045){
             echo "Credenciales incorrectas";
