85b5921fc7d467a6befa06580f1e3356dad3c6b6
Manuel <manuhg13@gmail.com>
2022-12-19 09:15:25 +0000

    d

---
+++ Tema5/sessiones/funciones/BD.php
@@ -0,0 +1,39 @@
+<?
+    require('../seguro/conexion.php');
+
+    function validaUser($user,$pass){
+        try {
+            $conexion= new PDO('mysql:host='. $_SERVER['SERVER_ADDR']. ';dbname=' .BBDD,USER,PASS);
+            $sql="select * from usuarios where usuario= ? and clave= ?;";
+            $sql_prepa= $conexion->prepare($sql);
+            $pass_e=sha1($pass);
+
+            $array= array($user,$pass_e);
+            $sql_prepa->execute();
+
+            //si devuelve algo hacemos un Login
+            if ($sql_prepa->rowCount()==1) {
+                session_start();
+                $_SESSION['validado']=true;
+                $row=$sql_prepa->fetch();
+                $_SESSION['user']=$user;
+                $_SESSION['nombre']=$row['nombre'];
+                $_SESSION['perfil']=$row['perfil'];
+
+                unset($conexion);
+                return true;
+            }else{
+                unset($conexion);
+                return false;
+
+            }
+            //sino pues no hacemos login
+
+        } catch (Exception $ex) {
+            print_r($ex);
+            unset($conexion);
+            
+        }
+    }
+
+?>
---
+++ Tema5/sessiones/funciones/validaciones.php
@@ -0,0 +1,20 @@
+<?
+    require("./BD.php");
+    $user=$_REQUEST['user'];
+    $pass=$_REQUEST['pass'];
+
+    if (validaUser($user,$pass)){
+        if ($_SESSION['perfil']=='ADM01') {
+            header("Location: ../paginas/admin.php");
+            exit;
+        }else{
+            header("Location: ../paginas/index.php");
+            exit;
+        }
+    }else{
+        header("Location: ../login.php");
+        exit;
+    }
+
+
+?>
