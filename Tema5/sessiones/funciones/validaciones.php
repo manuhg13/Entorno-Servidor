936f0ba69100e3615843db15984a623b2a185c17
Manuel <manuhg13@gmail.com>
2022-12-20 09:19:52 +0000

    8

--- Tema5/sessiones/funciones/validaciones.php
+++ Tema5/sessiones/funciones/validaciones.php
@@ -1,20 +1,26 @@
 <?
+    session_start();
     require("./BD.php");
     $user=$_REQUEST['user'];
     $pass=$_REQUEST['pass'];
-
-    if (validaUser($user,$pass)){
-        if ($_SESSION['perfil']=='ADM01') {
-            header("Location: ../paginas/admin.php");
-            exit;
-        }else{
-            header("Location: ../paginas/index.php");
-            exit;
-        }
-    }else{
+    if (empty($user)) {
+        $_SESSION['error']='Debe rellenar el nombre';
         header("Location: ../login.php");
         exit;
-    }
-
+    }else{
+        if (validaUser($user,$pass)){
+            if ($_SESSION['perfil']=='ADM01') {
+                header("Location: ../paginas/admin.php");
+                exit;
+            }else{
+                header("Location: ../paginas/index.php");
+                exit;
+            }
+        }else{
+            $_SESSION['error']='No existe el usuario';
+            header("Location: ../login.php");
+            exit;
+        }
+    }   
    
 ?>
--- Tema5/sessiones/login.php
+++ Tema5/sessiones/login.php
@@ -5,11 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <?
+        session_start();
+    ?>
 </head>
 <body>
-    <form action="./funciones/validaciones.php" method="get">
+    <?
+        if(isset($_SESSION['error'])){
+            echo $_SESSION['error'];
+        }
+        unset($_SESSION['error']);
+    ?>
+    <form action="./funciones/validaciones.php" method="post">
         <label for="user">Usuario</label>
         <input type="text" name="user" id="idUser">
+        <?
+           
+        ?>
         <label for="pass">Contraseña</label>
         <input type="password" name="pass" id="idPass">
         <input type="submit" value="Enviar" name="enviar">
