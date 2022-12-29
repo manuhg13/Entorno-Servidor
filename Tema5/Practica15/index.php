7670e84c09b234460a65ab508d9f9f73bf748173
manuel <manuhg13@gmail.com>
2022-12-29 12:25:26 +0000

    7

--- Tema5/Practica15/index.php
+++ Tema5/Practica15/index.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/principal.css">
     <title>INICIO || PR 15</title>
 </head>
 <body>
-    
+    <header>
+        <div class="logo">
+            <img src="" alt="">
+        </div>
+        <div class="botones">
+            <a href="" class="boton">Iniciar sesion</a>
+            <a href="" class="boton">Registrarse</a>
+        </div>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="./index.php" class="activo">Inicio</a></li>
+            <li><a href="./index.php">Tienda</a></li>
+            <li><a href="./index.php">Ayuda</a></li>
+        </ul>
+    </nav>
 </body>
 </html>
