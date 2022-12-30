57cf73550756f2a44b1a86c9cf9531e0889e4f40
manuel <manuhg13@gmail.com>
2022-12-30 11:54:18 +0000

    d

--- Tema5/Practica15/BBDD/tienda.sql
+++ Tema5/Practica15/BBDD/tienda.sql
@@ -29,13 +29,12 @@ create table productos(
     stock int(3) not null
 )engine=innodb;
 
-INSERT INTO `productos` (`nombre`,`precio`,`descripcion`,`stock`) VALUES
-('Paleta de bellota 5 Jotas','167.45','Paleta de bellota 100% ibérica 5 Jotas Jabugo Pata Negra','10'),
-('Jamón Etiqueta Negra Benito','133.65','El jamón Benito Etiqueta Negra Selección de Autor','3'),
-('Paletilla Serrana Manuel Díaz','28.00','Prestigiosa marca Manuel Díaz 50% raza Duroc','5'),
-('Jamón Serrano Reserva El Pozo','66.00','Jamón serrano Reserva Serie Oro  El Pozo. Satisfación Garantizada','12'),
-('Jamón Joselito 100%','475.00','Jamón Joselito 100% natural con más de 48 meses de curación.','2'),
-('Paletilla Serrana Bodega Los Romeros','30.00','Paleta Serrana Bodega Los Romeros','20');
+INSERT INTO productos ('nombre','precio','descripcion','stock') VALUES ('Paleta de bellota 5 Jotas','167.45','Paleta de bellota 100% ibérica 5 Jotas Jabugo Pata Negra','10'),
+INSERT INTO productos ('nombre','precio','descripcion','stock') VALUES ('Jamón Etiqueta Negra Benito','133.65','El jamón Benito Etiqueta Negra Selección de Autor','3'),
+INSERT INTO productos ('nombre','precio','descripcion','stock') VALUES ('Paletilla Serrana Manuel Díaz','28.00','Prestigiosa marca Manuel Díaz 50% raza Duroc','5'),
+INSERT INTO productos ('nombre','precio','descripcion','stock') VALUES ('Jamón Serrano Reserva El Pozo','66.00','Jamón serrano Reserva Serie Oro  El Pozo. Satisfación Garantizada','12'),
+INSERT INTO productos ('nombre','precio','descripcion','stock') VALUES ('Jamón Joselito 100%','475.00','Jamón Joselito 100% natural con más de 48 meses de curación.','2'),
+INSERT INTO productos ('nombre','precio','descripcion','stock') VALUES ('Paletilla Serrana Bodega Los Romeros','30.00','Paleta Serrana Bodega Los Romeros','20');
 
 create table ventas(
     idVenta int primary key auto_increment,
