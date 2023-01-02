7f2e5305047292e9129fe76127e37868ee2efe63
manuel <manuhg13@gmail.com>
2023-01-02 11:39:37 +0000

    d

--- Tema5/Practica15/sesiones/registro.php
+++ Tema5/Practica15/sesiones/registro.php
@@ -8,7 +8,48 @@
 </head>
 <body>
     <form action="./registro.php" method="post">
-        
+    <p>
+            <label for="idUser">User</label>
+            <input type="text" name="user" id="user" placeholder="Usuario" value="<?php
+                if (enviado() && !vacio("user")) {
+                    echo $_REQUEST['user'];
+                }
+            ?>">
+            <?php
+                if (vacio("user") && enviado()){
+                    echo "<p style='color: red'> Introduce el usuario</p>";
+                }
+            ?>
+        </p>
+        <p>
+            <label for="idContraseña">Contraseña: </label>
+            <input type="text" name="pass" id="idContraseña" placeholder="Contraseña">
+            
+            <?php
+                if (enviado()) {
+                    if (vacio('pass')) {
+                        echo "<p style='color: red'> Introduce la contraseña</p>";        
+                    }elseif (!patPass()) {
+                        echo "<p style='color: red'> Al menos una mayuscula, una minuscula y un número/p>";                         
+                    }
+                }
+                
+            ?>
+        </p>
+        <p>
+            <label for="idContraseña2">Repite la contraseña: </label>
+            <input type="text" name="pass2" id="idContraseña2" placeholder="Repite contraseña">
+            
+            <?php
+                if (enviado()){
+                    if (vacio('pass2')) {
+                        echo "<p style='color: red'> Introduce de nuevo la contraseña</p>";            
+                    }elseif ($_REQUEST['pass']!=$_REQUEST['pass2']) {
+                        echo "<p style='color: red'> Introduce de nuevo la contraseña correctamente</p>";                              
+                    }
+                }
+            ?>
+        </p>
     </form>
 </body>
 </html>
