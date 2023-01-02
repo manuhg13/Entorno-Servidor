0440c218d72fd200b9c2ef03a2a4599fb485eb18
manuel <manuhg13@gmail.com>
2023-01-02 11:41:33 +0000

    d

--- Tema5/Practica15/sesiones/registro.php
+++ Tema5/Practica15/sesiones/registro.php
@@ -8,23 +8,24 @@
 </head>
 <body>
     <form action="./registro.php" method="post">
-    <p>
-            <label for="idUser">User</label>
-            <input type="text" name="user" id="user" placeholder="Usuario" value="<?php
-                if (enviado() && !vacio("user")) {
-                    echo $_REQUEST['user'];
-                }
-            ?>">
-            <?php
-                if (vacio("user") && enviado()){
-                    echo "<p style='color: red'> Introduce el usuario</p>";
-                }
-            ?>
+        <p>
+                <label for="idUser">User</label>
+                <input type="text" name="user" id="user" placeholder="Usuario" value="<?php
+                    if (enviado() && !vacio("user")) {
+                        echo $_REQUEST['user'];
+                    }
+                ?>">
+                <?php
+                    if (vacio("user") && enviado()){
+                        echo "<p style='color: red'> Introduce el usuario</p>";
+                    }
+                ?>
         </p>
+
         <p>
             <label for="idContraseña">Contraseña: </label>
             <input type="text" name="pass" id="idContraseña" placeholder="Contraseña">
-            
+                
             <?php
                 if (enviado()) {
                     if (vacio('pass')) {
@@ -33,9 +34,10 @@
                         echo "<p style='color: red'> Al menos una mayuscula, una minuscula y un número/p>";                         
                     }
                 }
-                
+                    
             ?>
         </p>
+
         <p>
             <label for="idContraseña2">Repite la contraseña: </label>
             <input type="text" name="pass2" id="idContraseña2" placeholder="Repite contraseña">
@@ -46,6 +48,25 @@
                         echo "<p style='color: red'> Introduce de nuevo la contraseña</p>";            
                     }elseif ($_REQUEST['pass']!=$_REQUEST['pass2']) {
                         echo "<p style='color: red'> Introduce de nuevo la contraseña correctamente</p>";                              
+                    }
+                }
+            ?>
+        </p>
+
+        <p>
+            <label for="idEmail">Email: </label>
+            <input type="text" name="mail" id="idEmail" placeholder="Email" value="<?php
+                if (enviado() && !vacio('mail')) {
+                    echo $_REQUEST['mail'];
+                }
+            ?>">
+
+            <?php
+                if (enviado()) {
+                    if (vacio('mail')) {
+                        echo "<p style='color: red'>Introduce Email</p>";
+                    }elseif (!patMail()) {
+                        echo "<p style='color: red'>Introduce Email correcto</p>";     
                     }
                 }
             ?>
