71738b095aa4a45f58501436dab9e95a01f5589f
manuel <manuhg13@gmail.com>
2023-01-04 18:26:39 +0000

    q

--- Tema5/Practica15/sesiones/login.php
+++ Tema5/Practica15/sesiones/login.php
@@ -11,20 +11,42 @@
     ?>
 </head>
 <body>
+    <header>
+        <div class="logo">
+            <img src="../img/logo.png" alt="logo">
+        </div>
+        <div class="botones">
+            <a href="./sesiones/login.php" class="boton">Iniciar sesion</a>
+            <a href="./sesiones/registro.php" class="boton">Registrarse</a>
+        </div>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="../index.php" class="activo">Inicio</a></li>
+            <li><a href="./index.php">Tienda</a></li>
+            <li><a href="./index.php">Ayuda</a></li>
+        </ul>
+    </nav>
     <br>
-        <form action="../Funciones/validaciones.php" method="post">
-            <label for="user">Usuario</label>
-            <input type="text" name="user" id="idUser">
-            <label for="pass">Contraseña</label>
-            <input type="password" name="pass" id="idPass">
-            <input type="submit" value="Enviar" name="enviar">
-        </form>
+    <div class="ordenar">
+        <div class="caja">
+            <form action="../Funciones/validaciones.php" method="post">
+                <label for="user">Usuario</label>
+                <input type="text" name="user" id="idUser">
+                <br><br>
+                <label for="pass">Contraseña</label>
+                <input type="password" name="pass" id="idPass">
+                <br><br>
+                <input type="submit" value="Enviar" name="enviar" class="boton">
+            </form>
+        <?
+            if(isset($_SESSION['error'])){
+                echo "<p style='padding: 10px; color: red'>" . $_SESSION['error'] . "</p>";
+            }
+            unset($_SESSION['error']);
+        ?>  
+        </div>
+    </div>
 
-    <?
-        if(isset($_SESSION['error'])){
-            echo "<p style='padding: 10px; color: red'>" . $_SESSION['error'] . "</p>";
-        }
-        unset($_SESSION['error']);
-    ?>
 </body>
 </html>
