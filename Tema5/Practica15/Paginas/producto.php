07ff04171f34469adce893e472ffda63417f6011
manuel <manuhg13@gmail.com>
2023-01-04 19:16:12 +0000

    7

--- Tema5/Practica15/Paginas/producto.php
+++ Tema5/Practica15/Paginas/producto.php
@@ -8,26 +8,86 @@
     <title>Artículo</title>
 </head>
 <body>
+    <?
+         require("../Funciones/conexionBD.php");
+         require("../Funciones/funciones.php");
+
+        try {
+            $conexion=new PDO('mysql:host='. $_SERVER['SERVER_ADDR']. ';dbname=' .BBDD,USER,PASS);
+
+            $consulta= $conexion->query("SELECT * FROM productos");
+
+            $arrayJamones=array();
+
+            while ($fila= $consulta->fetch(PDO::FETCH_ASSOC)) {
+                array_push($arrayJamones,$fila);
+            }
+
+            
+        } catch (Exception $ex) {
+            if ($ex->getCode()==1045){
+                echo "Credenciales incorrectas";
+            }
+            if ($ex->getCode()==2002){
+                echo "Acabado tiempo de conexión";
+            }       
+            if ($ex->getCode()==1049){
+                echo "No hay BBDD";
+    
+            }      
+        }
+    ?>
+
     <header>
         <div class="logo">
-            <img src="img/logo.png" alt="logo">
+            <img src="../img/logo.png" alt="logo">
         </div>
         <div class="botones">
             <?
                 session_start();
                 if (estaValidado()) {
-                    echo '<a href="./sesiones/login.php" class="boton">'.$_SESSION['user'].'</a>';
-                    echo '<a href="./sesiones/logout.php" class="boton">Cerrar sesión</a>';
+                    echo '<a href="../sesiones/login.php" class="boton">'.$_SESSION['user'].'</a>';
+                    echo '<a href="../sesiones/logout.php" class="boton">Cerrar sesión</a>';
                 }else {
                     
                 
             ?>
-                <a href="./sesiones/login.php" class="boton">Iniciar sesion</a>
-                <a href="./sesiones/registro.php" class="boton">Registrarse</a>
+                <a href="../sesiones/login.php" class="boton">Iniciar sesion</a>
+                <a href="../sesiones/registro.php" class="boton">Registrarse</a>
             <?
                 }
             ?>
         </div>
     </header>
+    <nav>
+        <ul>
+            <li><a href="../index.php">Inicio</a></li>
+            <li><a href="../index.php" class="activo">Tienda</a></li>
+            <li><a href="../index.php">Ayuda</a></li>
+        </ul>
+    </nav>
+    <div class="ordenar">
+        <main>            
+            <section>
+                
+                <?
+                    foreach ($arrayJamones as $jamon) {
+                        if ($jamon['idProducto']==$_REQUEST['id']){
+                            echo "<article>";
+                                echo '<img src="../'. $jamon['url'].'" alt="fotoJamón">';
+                                echo '<h3>'. $jamon['nombre']. '</h3>';
+                                echo '<p>Descripcion: '.$jamon['descripcion'].'</p>';
+                                echo '<p>Precio: '.$jamon['precio'].'€</p>';
+                                echo '<a href="../Paginas/producto.php?id='.$jamon['idProducto'].'" class="boton">COMPRAR YA</a>';                        
+                            echo "</article>";
+                        }
+                    }
+
+                ?>
+            </section>
+        </main>
+    </div>
+
+
 </body>
 </html>
