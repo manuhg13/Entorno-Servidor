0f0d02cbecd35dc356f602826a397aa8d066a847
manuel <manuhg13@gmail.com>
2023-01-11 19:08:16 +0000

    7

--- Tema5/Practica15/Funciones/funciones.php
+++ Tema5/Practica15/Funciones/funciones.php
@@ -153,4 +153,21 @@
         }
     }
 
+    function findAll(){
+        try{
+            $conexion=new PDO('mysql:host='. $_SERVER['SERVER_ADDR']. ';dbname=' .BBDD,USER,PASS);
+            $sql="select * from ventas";
+            $resultado=$conexion->query($sql);
+            $array=$resultado->fetchAll(PDO::FETCH_ASSOC);
+           
+            unset($conexion);
+            return $array;
+           
+        }catch(Exception $ex){
+            print_r($ex);
+            unset($conexion);
+            return false;
+        }
+    }
+
 ?>
