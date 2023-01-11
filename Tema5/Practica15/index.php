ed0983944017405dd3cf2fca74def11f974453da
manuel <manuhg13@gmail.com>
2023-01-11 16:26:13 +0000

    8

--- Tema5/Practica15/index.php
+++ Tema5/Practica15/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/principal.css">
+    <link rel="stylesheet" href="./CSS/principal.css">
     <link href="./CSS/bootstrap.min.css" rel="stylesheet">
     <link href="./CSS/headers.css" rel="stylesheet">
     <link href="./CSS/carousel.css" rel="stylesheet">
