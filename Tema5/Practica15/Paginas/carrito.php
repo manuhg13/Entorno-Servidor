66734f3318f55ea689a1d4b53e03e0bb7c6bb86e
manuel <manuhg13@gmail.com>
2023-01-11 18:26:30 +0000

    e

--- Tema5/Practica15/Paginas/carrito.php
+++ Tema5/Practica15/Paginas/carrito.php
@@ -24,27 +24,5 @@
                 }
             ?>
         
-        <?
-            try {
-                $conexion=new PDO('mysql:host='. $_SERVER['SERVER_ADDR']. ';dbname=' .BBDD,USER,PASS);
-
-                $consulta= $conexion->query("SELECT * FROM productos where idProducto='".$_REQUEST['id']."'");
-
-                unset($conexion);
-            } catch (Exception $ex) {
-                if ($ex->getCode()==1045){
-                    echo "Credenciales incorrectas";
-                }
-                if ($ex->getCode()==2002){
-                    echo "Acabado tiempo de conexión";
-                }       
-                if ($ex->getCode()==1049){
-                    echo "No hay BBDD";
-        
-                }      
-            }
-        ?>
-
-    </div>
 </body>
 </html>
