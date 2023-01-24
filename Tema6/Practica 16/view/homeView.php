c3df38d5f414d69fbe98be3c6b0c4596a1858774
manuel <manuhg13@gmail.com>
2023-01-24 21:24:38 +0000

    7

--- Tema6/Practica 16/view/homeView.php
+++ Tema6/Practica 16/view/homeView.php
@@ -1,47 +1,82 @@
 <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="./img/cerdis.jpg" alt="Los Angeles" class="d-block w-100">
-        <div class="container">
-            <div class="carousel-caption text-start">
-            <h1>Criados con cariño</h1>
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/cerdis.jpg" alt="Los Angeles" class="d-block w-100">
+      <div class="container">
+        <div class="carousel-caption text-start">
+          <h1>Criados con cariño</h1>
         </div>
-    </div>
       </div>
-      <div class="carousel-item">
+    </div>
+    <div class="carousel-item">
       <img src="./img/dehesa.jpg" alt="Los Angeles" class="d-block w-100">
 
-        <div class="container">
-          <div class="carousel-caption">
-            <h1>100% raza Iberica</h1>
-            <p>Alimentados en las mejores dehesas</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Leer más</a></p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-      <img src="./img/curando.jpg" alt="curando Jamones" class="d-block w-100">
-        <div class="container">
-          <div class="carousel-caption text-end">
-            <h1>Proceso de curacion natural</h1>
-            <p>Al más puro estilo tradicional</p>
-          </div>
+      <div class="container">
+        <div class="carousel-caption">
+          <h1>100% raza Iberica</h1>
+          <p>Alimentados en las mejores dehesas</p>
+          <p><a class="btn btn-lg btn-primary" href="#">Leer más</a></p>
         </div>
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+    <div class="carousel-item">
+      <img src="./img/curando.jpg" alt="curando Jamones" class="d-block w-100">
+      <div class="container">
+        <div class="carousel-caption text-end">
+          <h1>Proceso de curacion natural</h1>
+          <p>Al más puro estilo tradicional</p>
+        </div>
+      </div>
+    </div>
   </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
-  
+<div class="col-12 text-center">
+  <p class="diplay-4">Los más deseados</p>
+</div>
+
+<section class="album py-5">
+  <section class="container">
+    <section class='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
+      <?php
+      $arrayJamones=ProductoDAO::findAll();
+
+      foreach ($arrayJamones as $jamon) {
+        if ($jamon->precio >= 100) {?>
+          <section class='col'>
+          <section class='card shadow-sm'>
+          <img src='<? echo $jamon->url ?>' width='100%' height='450'>
+          <section class='card-body bg-dark'>
+          <h3 class='h3 text-light'><? echo $jamon->nombre ?></h3>
+          <p class='card-text text-light'><? echo $jamon->descripcion ?></p>
+          <section class='d-flex justify-content-between align-items-center'>";
+          <section class='btn-group'>";
+            <form action="./index.php" method="post">
+              <input type="hidden" name="idProducto" value="<? echo $jamon->idProducto ?>">
+              <input type="submit" value="Ver" name="ver" class="btn btn-danger">
+            </form>
+          </section>";
+          <small class='text-light'><? echo $jamon->precio ?>€</small>";
+          </section>";
+          </section>";
+          </section>";
+          </section>";
+        <?}
+      }
+      ?>
+    </section>
+  </section>
+</section>
