8fb428ab1c33558b57ebd5b5804ae523e460b2ab
Manuel <manuhg13@gmail.com>
2023-01-24 08:47:12 +0000

    d

---
+++ Tema6/Practica 16/config/tienda.sql
@@ -0,0 +1,70 @@
+create database tienda;
+use tienda;
+
+create table roles(
+    id char(5) primary key,
+    descripcion char(30)
+) engine=innodb;
+
+INSERT INTO roles VALUES ('ADM01','Administrador');
+INSERT INTO roles VALUES ('MOD02','Moderador');
+INSERT INTO roles VALUES ('NOR03','Usuario normal');
+
+create table usuarios(
+    usuario char(20) primary key,
+    clave char(40) not null,
+    nombre varchar(75) not null,
+	correo varchar(75) not null,
+    fecha date not null,
+	roles char(5) not null,
+	index (roles),
+	foreign key (roles) references roles (id)
+)engine=innodb;
+
+/*Contraseña: Adm1234567*/
+INSERT INTO usuarios VALUES ('admin','8c9c8ccfcbf6e09beb44b8e25841037ee2292cfe','admin','admin@admin.com','2023-01-15','ADM01');
+/*Contraseña: Mod1234567*/
+INSERT INTO usuarios VALUES ('mod','aa0e39d753ded291efa2454e91b40b18442af243','mod','mod@mod.com','2023-01-15','MOD02');
+/*Contraseña: Nor1234567*/
+INSERT INTO usuarios VALUES ('normi','1cc75cf56f6263ee1af243787705c56688071e59','normi','normi@normi.com','2023-01-15','NOR03');
+
+create table productos(
+    idProducto int primary key auto_increment,
+    nombre char(60) not null,
+    precio numeric(5,2) not null,
+    descripcion char(85) not null,
+    stock int(3) not null,
+    url char(30) not null
+)engine=innodb;
+
+INSERT INTO productos (nombre,precio,descripcion,stock,url) VALUES ('Paleta de bellota 5 Jotas','167.45','Bellota 100% ibérica 5 J ','10','./img/bellota5j.png');
+INSERT INTO productos (nombre,precio,descripcion,stock,url) VALUES ('Jamon Benito','133.65','El jamon Benito Etiqueta Negra Seleccion de Autor','3','./img/benito.png');
+INSERT INTO productos (nombre,precio,descripcion,stock,url) VALUES ('Paletilla Serrana Manuel Diaz','28.00','Prestigiosa marca Manuel Diaz 50% raza Duroc','5','./img/manuel.jpg');
+INSERT INTO productos (nombre,precio,descripcion,stock,url) VALUES ('Jamon Reserva El Pozo','66.00','Jamón serrano Reserva Serie Oro  El Pozo.','12','./img/elpozo.jpg');
+INSERT INTO productos (nombre,precio,descripcion,stock,url) VALUES ('Jamon Joselito 100%','475.00','Jamon Joselito 100% natural con mas de 48 meses de curacion.','2','./img/joselito.jpg');
+INSERT INTO productos (nombre,precio,descripcion,stock,url) VALUES ('Paletilla Bodega Los Romeros','30.00','Paleta Serrana Bodega Los Romeros','20','./img/romeros.jpg');
+
+create table ventas(
+    idVenta int primary key auto_increment,
+    cliente char(20) not null,
+    fechaVent date not null,
+    idProducto int not null,
+    cantidad int not null,
+    precioTotal numeric(7,2) not null,
+    index(cliente),
+    foreign key (cliente) references usuarios(usuario),
+    index(idProducto),
+    foreign key (idProducto) references productos(idProducto)
+)engine=innodb;
+
+create table albaran(
+    idAlbaran int primary key auto_increment,
+    fechaAlbaran date not null,
+    idProducto int not null,
+    cantidad int not null,
+    usuario char(20) not null,
+    index(idProducto),
+    foreign key (idProducto) references productos(idProducto),
+    index(usuario),
+    foreign key (usuario) references usuarios(usuario)
+)engine=innodb;
---
+++ Tema6/Practica 16/model/Ventas.php
@@ -0,0 +1,37 @@
+<?
+    class Ventas{
+        private $idVenta;
+        private $cliente;
+        private $fechaVent;
+        private $idProducto;
+        private $cantidad;
+        private $precioTotal;
+
+        public function __construct($idVenta,$cliente,$fechaVent,$idProducto,$cantidad,$precioTotal)
+        {
+            $this->idVenta=$idProducto;
+            $this->cliente=$cliente;
+            $this->fechaVent=$fechaVent;
+            $this->idProducto=$idProducto;
+            $this->cantidad=$cantidad;
+            $this->precioTotal=$precioTotal;
+        }
+
+        public function __get($get)
+        {
+            //Si la propiedad no existiera retornaría null
+            if (property_exists(__CLASS__,$get)) {
+                return $this->$get;
+            }
+            return null;
+        }
+
+        public function __set($clave, $valor)
+        {
+            if (property_exists(__CLASS__,$clave)) {
+                return $this->$clave=$valor;
+            }
+        } 
+    }
+
+?>
