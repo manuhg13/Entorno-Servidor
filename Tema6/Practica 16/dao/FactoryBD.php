e8080092e957401b3d1c5de5c06f074d93135ab6
manuel <manuhg13@gmail.com>
2023-01-26 16:28:42 +0000

    d

--- Tema6/Practica 16/dao/FactoryBD.php
+++ Tema6/Practica 16/dao/FactoryBD.php
@@ -12,11 +12,15 @@
             } catch (Exception $ex) {
                 //Si no va pues nulo
                 $preparada=null;
+                $conexion2= new PDO('mysql:host='. $_SERVER['SERVER_ADDR'] ,USER,PASS);
+                $script=file_get_contents('./config/tienda.sql');
+                $conexion2->exec($script);
                 echo $ex;
             }finally{
                 unset($con);
                 return $preparada;
             }
         }
+
     }
 ?>
