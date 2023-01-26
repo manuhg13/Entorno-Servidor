1f202ca846d66e373237ba36be9e7c150f1a8e1c
manuel <manuhg13@gmail.com>
2023-01-26 16:14:10 +0000

    8

--- Tema6/Practica 16/view/layout.php
+++ Tema6/Practica 16/view/layout.php
@@ -35,7 +35,6 @@
               <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
             </svg>
           </a>
-          <form action=""></form>
           <ul class="dropdown-menu text-small">
             <li><input type="submit" class="dropdown-item" name="miperfil" value="Perfil"></li>
             <? if (esAdmin() || esModerador()) { ?>
