20fcca0e97a5701fe73ffe28e124365a86f7a113
manuel <manuhg13@gmail.com>
2023-01-27 15:27:19 +0000

    7

--- Tema6/Practica 16/view/userView.php
+++ Tema6/Practica 16/view/userView.php
@@ -1,55 +1,67 @@
 <div class="container">
     <form action="./index.php" method="post">
+        <!--Nombre-->
         <div class="mb-3 row">
-            <label for="inputName" class="col-4 col-form-label">Nombre</label>
+            <label for="nombre" class="col-4 col-form-label">Nombre</label>
             <div class="col-8">
-                <input type="text" class="form-control" name="nombre" id="inputName" placeholder="Name" value="<? echo $usuario->nombre?>">
+                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre" value="<? echo $usuario->nombre ?>">
             </div>
         </div>
+        <!--Usuario-->
         <div class="mb-3 row">
-            <label for="inputName" class="col-4 col-form-label">Usuario</label>
+            <label for="user" class="col-4 col-form-label">Usuario</label>
             <div class="col-8">
-                <input type="text" class="form-control" name="user" id="inputName" placeholder="Name" value="<? echo $usuario->usuario?>">
+                <input type="text" class="form-control" name="user" id="user" placeholder="Usuario" value="<? echo $usuario->usuario ?>" readonly>
             </div>
         </div>
-
-        <?if ($_SESSION['accion']=='editar'){?>
-        <div class="mb-3 row">
-            <label for="inputName" class="col-4 col-form-label">Contraseña</label>
-            <div class="col-8">
-                <input type="pass" class="form-control" name="pass" id="inputName" placeholder="Name" value="<? echo $usuario->clave?>">
+        <!--Contraseña-->
+        <? if ($_SESSION['accion'] == 'editar') { ?>
+            <div class="mb-3 row">
+                <label for="pass" class="col-4 col-form-label">Contraseña</label>
+                <div class="col-8">
+                    <input type="pass" class="form-control" name="pass" id="pass" placeholder="Contraseña">
+                </div>
             </div>
-        </div>
+            <div class="mb-3 row">
+                <label for="pass2" class="col-4 col-form-label">Repetir contraseña</label>
+                <div class="col-8">
+                    <input type="pass" class="form-control" name="pass2" id="pass2" placeholder="Repite la contraseña">
+                </div>
+            </div>
         <?
-            }
+        }
         ?>
-        
+        <!--Email-->
         <div class="mb-3 row">
-            <label for="inputName" class="col-4 col-form-label">Email</label>
+            <label for="email" class="col-4 col-form-label">Email</label>
             <div class="col-8">
-                <input type="text" class="form-control" name="email" id="inputName" placeholder="Name" value="<? echo $usuario->correo?>">
+                <input type="text" class="form-control" name="email" id="email" placeholder="Correo" value="<? echo $usuario->correo ?>">
             </div>
         </div>
+        <!--Rol-->
         <div class="mb-3 row">
-            <label for="inputName" class="col-4 col-form-label">Perfil</label>
-            <div class="col-8">
-                <input type="text" class="form-control" name="perfil" id="inputName" placeholder="Name" value="<? echo $usuario->perfil?>">
-            </div>
+            <label for="rol" class="form-label">Rol</label>
+            <select class="form-select form-select-lg" name="roles" id="rol">
+                <option selected value="0">Selecciona un rol</option>
+                <option value="ADM01">Administrador</option>
+                <option value="MOD02">Moderador</option>
+                <option value="NOR03">Usuario normal</option>
+            </select>
         </div>
-        
+        <!--Submits-->
         <div class="mb-3 row">
             <div class="offset-sm-4 col-sm-8">
                 <?
-                    if ($_SESSION['accion']=='editar') {?>
-                        <input type="submit" class="btn btn-primary" name="guardar" value="Guardar"/>
-                        
-                    <?}
+                if ($_SESSION['accion'] == 'editar') { ?>
+                    <input type="submit" class="btn btn-primary" name="guardar" value="Guardar" />
+
+                <? }
                 ?>
                 <?
-                    if ($_SESSION['accion']=='ver') {?>
-                        <input type="submit" class="btn btn-primary" name="editar" value="Editar"/>
-                        
-                    <?}
+                if ($_SESSION['accion'] == 'ver') { ?>
+                    <input type="submit" class="btn btn-primary" name="editar" value="Editar" />
+
+                <? }
                 ?>
             </div>
         </div>
