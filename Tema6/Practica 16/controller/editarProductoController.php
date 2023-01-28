4cc40f7bdf8a2a1c3864480077e64e860567c25a
manuel <manuhg13@gmail.com>
2023-01-28 13:13:56 +0000

    q

--- Tema6/Practica 16/controller/editarProductoController.php
+++ Tema6/Practica 16/controller/editarProductoController.php
@@ -2,7 +2,7 @@
 if (isset($_REQUEST['idProducto'])){
     $_SESSION['producto']=$_REQUEST['idProducto'];
     $producto=ProductoDAO::findById($_SESSION['producto']);
-}if (isset($_REQUEST['guardar'])){
+}elseif (isset($_REQUEST['guardar'])){
     if ( $_SESSION['accion']=='editar'){
         $producto=ProductoDAO::findById($_SESSION['producto']);
         $producto->precio=(float)$_REQUEST['precio'];
