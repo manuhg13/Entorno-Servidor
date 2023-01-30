8591ef88f9e95c78c552e3092e2cef25059ee742
manuel <manuhg13@gmail.com>
2023-01-30 17:19:01 +0000

    7

--- Tema6/Practica 16/controller/ventasController.php
+++ Tema6/Practica 16/controller/ventasController.php
@@ -2,6 +2,11 @@
     if (isset($_REQUEST['ventas'])) {
         $ventas=VentasDAO::findAll();
     }elseif (isset($_REQUEST['eliminar'])) {
+        VentasDAO::delete($_REQUEST['idVenta']);
+    }elseif (isset($_REQUEST['editar'])){
+        $_SESSION['vista']=$vistas['ventas'];
+    }elseif (isset($_REQUEST['guardar'])) {
         $venta=VentasDAO::findById($_REQUEST['idVenta']);
+        
     }
 ?>
